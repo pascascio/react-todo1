81eea6ecdb36a0f9afa6f4e18c5aedbffb15b60a
Kenya <pascasciokenya97@gmail.com>
2023-06-06 16:28:10 -0400

    created unorder todo list with function component and JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
+
+const todoList = [
+{
+  id: 1,
+  title: "Take clothes to dry cleaners",
+},
+{
+  id: 2,
+  title: "Confirm Appointment with Dr.Jones",
+},
+{
+  id: 3,
+  title: "RSVP for Parker's Piano Recital",
+},
+];
+
+
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+          {
+           todoList.map(function(item){
+                return <li key={item.id}>{item.title}</li>
+            })
+          }
+      </ul>
     </div>
   );
 }
+
 
 export default App;
